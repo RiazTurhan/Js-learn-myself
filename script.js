45fd09b56ee74db12f6d39d25343069154a6c015
RiazTurhan <turhanuddin19711@gmail.com>
2022-06-28 23:13:50 +0600

    js

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ let LastName = "Turhan";
 let FullName = FirstName + " " + LastName;
 document.write(FullName);
 
-const cars = [ "BMW", "VOLVO", "Corola"];
-document.write(cars[1]);
+const cars = [ "BMW", "VOLVO", "Corola"]; //this is array
+document.write(cars[1]); // call arry one element
 
 if (x > 20) {
     document.write("It's Small")
 } else {
     document.write("It's Big")
 }
-function carsValu (prize){
+
+function carsValu (prize){ //function use and if and else use inside the function element
      console.log(prize + " " + "Doller I pay you")
     if (prize > 100 ){
        console.log("you will buy this car");
@@ -25,25 +26,38 @@ function carsValu (prize){
     }
 }
 
-carsValu (1020);
+carsValu (1020); //function call
 
 function Timedate (){
     document.getElementById("demo").innerHTML = Date();
 }
 
 let mongo = 123.44;
-console.log(typeof mongo.toExponential(2));
+console.log(typeof mongo.toExponential(2)); //number methods
 Number(mongo);
 console.log(mongo);
 
 const Fruits = ["Mango", "Orange", "Bananna"]
 console.log(Fruits);
 console.log(Fruits[0]);
-Fruits[0]= "Nothing";
+// Fruits[0]= "Nothing";
 console.log(Fruits);
 
 function forUse (value){
     console.log(value);
 }
-Fruits.forEach(forUse);
+Fruits.forEach(forUse); // this methods work like for loop
 console.log("finished");
+
+Fruits.push("Lemone"); //new element added 
+console.log(Fruits[3]); 
+
+const person = []; //If you use named indexes, JavaScript will redefine the array to an object.After that, some array methods and properties will produce incorrect results.
+person["firstName"] = "John";
+person["lastName"] = "Doe";
+person["age"] = 46;
+person.length;
+person[0];   
+console.log(person);  //Arrays are a special kind of objects, with numbered indexes.
+
+console.log(Array.isArray(person)); //solution for array and object ideantifi 
